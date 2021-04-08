c946cb96fd12c5b0256e1c2a4d2114c43ddfeb1a
Vladymyr Radionov <radionovdon@gmail.com>
2021-04-08 14:22:51 +0300

    dropdown

---
+++ smart-plus/app/src/js/dropdown.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//declare variables
+const dropBtn = document.querySelectorAll('.dropdown__btn'),
+      dropCont = document.querySelectorAll('.dropdown__content'),
+      menuItems = document.querySelectorAll('.dropdown__item');
+
+
+//show/hide menu by click
+dropBtn.forEach(
+    v => v.addEventListener('click', function () {
+        this.closest('.dropdown').children[1].classList.toggle('show');
+    })
+)
+
+//hide menu by click out
+window.addEventListener('click', function (e) {
+    if (!e.target.classList.contains('dropdown__btn')) {
+        dropCont.forEach(
+            it => it.classList.remove('show')
+        )
+    }
+});
+
+//change value in dropdown button
+menuItems.forEach(
+    it => it.addEventListener('click', function () {
+        this.closest('.dropdown').children[0].innerText = this.innerText;
+    })
+)
